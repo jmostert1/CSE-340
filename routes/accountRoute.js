1a73125638f879b9fd0c0a76ca8238083e8b30f5
jmostert1 <joshuamostert9@gmail.com>
2025-09-23 13:03:37 +0200

    Implement user authentication with login and registration views, controllers, and routes; add session management and flash messaging

---
+++ routes/accountRoute.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController.js")
+
+// Route to build login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router
