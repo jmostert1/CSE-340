e6bf49d1a212dfddf34fe687ac4409b201f54b9d
jmostert1 <joshuamostert9@gmail.com>
2025-10-01 12:02:33 +0200

    Update owner of account_type to cse340jm4

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340jm3;
+    OWNER TO cse340jm4;
 
 --2) Table structure for table `classification`
 CREATE TABLE public.classification (
